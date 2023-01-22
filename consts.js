095a2d62e54b94abb42bfadd93b2887cbb167a8c
loki87by <loki87.666@gmail.com>
2023-01-22 20:14:34 +0300

    feat: add score

--- consts.js
+++ consts.js
@@ -1,5 +1,12 @@
 export const CANVAS = document.getElementById("game");
+export const FIRST_INFO = document.querySelector("section");
+export const FIRST_SCORE = document.getElementById("first-score");
+export const SECOND_INFO = document.getElementById("second-info");
+export const SECOND_SCORE = document.getElementById("second-score");
+export const POPUP = document.querySelector(".popup");
+export const POPUP_TITLE = POPUP.querySelector("h1");
 export const CONTEXT = CANVAS.getContext("2d");
+export const RESTART = POPUP.querySelector("button");
 export const GRID = 15;
 export const PADDLE_HEIGHT = GRID * 5;
 export const MAX_PADDLE_Y = CANVAS.height - GRID - PADDLE_HEIGHT;
@@ -12,3 +19,9 @@ export const COLLIDES = (obj1, obj2) => {
     obj1.y + obj1.height > obj2.y
   );
 };
+
+export function CLONE_POINTS(parent) {
+  const current = parent.querySelector('.point')
+  const copy = current.cloneNode(true)
+  parent.appendChild(copy)
+}
